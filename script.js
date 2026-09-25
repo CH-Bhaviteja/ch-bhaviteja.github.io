5fa35d5b16de6781039ceeef074afaa2c986d813
CH-Bhaviteja <t23115244@gmail.com>
2026-09-25 19:48:00 +0530

    Add theme toggle feature to script.js
    Implement theme toggle functionality with local storage support.

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("Bhaviteja Portfolio Loaded");
+const themeToggle = document.getElementById("theme-toggle");
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+    document.body.classList.add("dark-theme");
+    themeToggle.textContent = "☀️";
+} else {
+    document.body.classList.remove("dark-theme");
+    themeToggle.textContent = "🌙";
+}
+
+
+themeToggle.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark-theme");
+
+    if (document.body.classList.contains("dark-theme")) {
+
+        themeToggle.textContent = "☀️";
+
+        localStorage.setItem("theme", "dark");
+
+    } else {
+
+        themeToggle.textContent = "🌙";
+
+        localStorage.setItem("theme", "light");
+
+    }
+
+});
